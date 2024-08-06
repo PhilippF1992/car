9314e925ee0d280152d650311a734dbea4d75e3c
PhilippF1992 <philippfreese92@googlemail.com>
2024-08-06 12:33:46 +0200

    blinka test

--- headunit/setup.sh
+++ headunit/setup.sh
@@ -4,19 +4,26 @@
 echo 'Updating System: Start'
 sudo apt update
 sudo apt upgrade -y
-
 echo 'Updating System: Done'
-#setup raspi-config
-echo 'Configure raspi-config'
 
+#setup raspi-config
+echo 'Configure raspi-config: Start'
 sudo raspi-config nonint do_expand_rootfs
 sudo raspi-config nonint do_serial_hw 0
 sudo raspi-config nonint do_i2c 0
-#add docker
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_onewire 0
+echo 'Configure raspi-config: Start'
 
+#setup dtoverlays
+echo 'Setup dtoverlays: Start'
+sudo bash -c  "echo -en 'dtoverlay=w1-gpio,gpiopin=4' >> /boot/firmware/config.txt"
+sudo bash -c  "echo -en 'dtoverlay=pwm-2chan' >> /boot/firmware/config.txt"
+echo 'Setup dtoverlays: Done'
+
+#add docker
 echo 'Installing Docker: Start'
 #add GPG Key
-
 echo 'Adding GPG-Key: Start'
 mkdir /home/$USER/docker-volumes
 sudo apt-get install ca-certificates curl
@@ -24,7 +31,6 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo 'Adding GPG-Key: Done'
-
 #add repository
 echo 'Adding Docker Repository: Start'
 echo \
@@ -33,12 +39,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 echo 'Adding Docker Repository: Done'
-
 #install docker
 echo 'Installing Docker from Repository: Start'
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo 'Installing Docker from Repository: Done'
-
 #permissions docker
 echo 'Seting Permissions for Docker: Start'
 sudo groupadd docker
@@ -48,7 +52,6 @@ echo 'Seting Permissions for Docker: Done'
 echo 'Installing Docker: Done'
 
 #HomeAssistant
-
 echo 'Installing HomeAssistant: Start'
 mkdir /home/$USER/docker-volumes/home-assistant
 docker pull homeassistant/home-assistant:stable
@@ -68,10 +71,9 @@ sudo bash -c  "echo -en '  use_x_forwarded_for: true\n' >> /home/$USER/docker-vo
 sudo bash -c  "echo -en '  trusted_proxies:\n' >> /home/$USER/docker-volumes/home-assistant/configuration.yaml"
 sudo bash -c  "echo -en '    - 199.22.2.0/24\n' >> /home/$USER/docker-volumes/home-assistant/configuration.yaml"
 sudo bash -c  "echo -en '    - 10.253.0.1/24\n' >> /home/$USER/docker-volumes/home-assistant/configuration.yaml"
-
 echo 'Installing HomeAssistant: Done'
-#Mosquitto
 
+#Mosquitto
 echo 'Installing Mosquitto: Start'
 docker pull eclipse-mosquitto
 mkdir /home/$USER/docker-volumes/mosquitto
@@ -126,15 +128,10 @@ sudo useradd -r -s /bin/false python_car
 sudo adduser python_car gpio
 echo 'Setup Service User: Done'
 
-echo 'Setup dtoverlays: Start'
-sudo bash -c  "echo -en 'dtoverlay=w1-gpio,gpiopin=4' >> /boot/firmware/config.txt"
-sudo bash -c  "echo -en 'dtoverlay=pwm-2chan' >> /boot/firmware/config.txt"
-echo 'Setup dtoverlays: Done'
-
-echo 'Setup dtoverlays: Start'
+echo 'Setup Code: Start'
 sudo apt install git -y
 git clone https://github.com/PhilippF1992/car.git
-echo 'Setup dtoverlays: Done'
+echo 'Setup Code: Done'
 
 ##TODO Configure.py
 echo 'Reboot'
